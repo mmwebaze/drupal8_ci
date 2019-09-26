5ada75a7ea049dc0dd28207c29bc54748b419221
mwebaze <mmwebaze@ctacorp.com>
2019-09-26 13:18:37 -0400

    added branches script

---
+++ src/branches.groovy
@@ -0,0 +1,27 @@
+#!groovy
+
+import groovy.json.JsonSlurper;
+
+def call (String repoUrl) {
+
+    try {
+
+        List<String> params = new ArrayList<String>();
+        params.add("None");
+        //Getting branches
+        //URL apiUrl = "https://api.github.com/repos/mmwebaze/jenkins_demo/branches".toURL();
+        //Getting tags
+        //URL apiUrl = "https://api.github.com/repos/mmwebaze/jenkins_demo/tags".toURL();
+        URL apiUrl = "${repoUrl}".toURL();
+        List json = new JsonSlurper().parse(apiUrl.newReader());
+
+        for (repo in json) {
+
+            params.add(repo.name);
+        }
+        return params;
+    }
+    catch (IOException ex) {
+        print ex;
+    }
+}
