bec2a7c46ca5423402f604e44d5cb58d2ab77ab3
mwebaze <mmwebaze@ctacorp.com>
2019-09-26 08:09:36 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# drupal8_ci
+
+This is a sample Drupal 8 Jenkins pipeline library.
---
+++ vars/testPipeline.groovy
@@ -0,0 +1,7 @@
+#!/usr/bin/env groovy
+
+// vars/testPipeline.groovy
+
+def call () {
+    echo "Jenkins job is successful."
+}
